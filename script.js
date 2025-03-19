b249ff1eda509117d3aca269fb2573adf5ee6c85
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-03-19 19:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+ function removeColor() {
+    const selectElement = document.getElementById("colorSelect");
+    const selectedIndex = selectElement.selectedIndex;
+    selectElement.remove(selectedIndex);
+  }
